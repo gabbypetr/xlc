4235c17aacb2022b1eeb1d72e12918bb5f965533
gabbypetr <gabbypetr@proton.me>
2026-07-18 22:56:37 +0300

    Refactor Exporter class and remove unused code

---
+++ exporter.py
@@ -0,0 +1,22 @@
+class Exporter:
+
+    def save(
+        self,
+        formatted
+    ):
+
+        print()
+
+        print(
+
+            "Formatted JSON"
+
+        )
+
+        print()
+
+        print(
+
+            formatted
+
+        )
