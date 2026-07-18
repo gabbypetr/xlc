9c3578bb3591cfd0bfcbe6d2880ed995a95303bf
gabbypetr <gabbypetr@proton.me>
2026-07-18 22:57:25 +0300

    Create report.py

---
+++ report.py
@@ -0,0 +1,27 @@
+def summary(valid):
+
+    print()
+
+    print(
+
+        "-" * 30
+
+    )
+
+    print()
+
+    if valid:
+
+        print(
+
+            "Validation successful"
+
+        )
+
+    else:
+
+        print(
+
+            "Invalid JSON"
+
+        )
