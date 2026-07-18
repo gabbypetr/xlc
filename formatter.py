5090f0277b5b2327f8a972071c30bff230b9a17d
gabbypetr <gabbypetr@proton.me>
2026-07-18 22:50:45 +0300

    Update formatter.py

--- formatter.py
+++ formatter.py
@@ -1,1 +1,30 @@
 import json
+
+from settings import (
+    INDENT,
+    SORT_KEYS
+)
+
+
+class Formatter:
+
+    def format(
+        self,
+        document
+    ):
+
+        data = json.loads(
+
+            document.text
+
+        )
+
+        return json.dumps(
+
+            data,
+
+            indent=INDENT,
+
+            sort_keys=SORT_KEYS
+
+        )
